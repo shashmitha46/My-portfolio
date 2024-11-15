3e36dd00410c6a92000314c10d0f9b61d55b8c57
shashmitha46 <itzzshashmitha@gmail.com>
2024-11-15 21:53:48 +0530

    changed the allignment

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: ["AI Enthusiast", "Web Developer", "ML Practitioner", "AI & DS Student", "Data Enthusiast"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: ["AI Enthusiast", "Web Developer", "ML Practitioner", "AI & DS Student", "Data Enthusiast"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
